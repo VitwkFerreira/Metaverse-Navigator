17f8f3934f709ecb2dc77334b9ef7d136dc4f3fd
VitwkFerreira <vitwk@hotmail.com>
2019-07-10 23:45:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -34,4 +34,4 @@ client.on('message', message => {
  	}
 });
 
-client.login(token);
+client.login(process.env.BOT_TOKEN);
